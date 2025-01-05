ef66329f9fc2065a3f83dfc8eb1c5f7582055ee5
Marton Takacs <marton.takacs@tesco.com>
2025-01-05 14:22:00 +0100

    menu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,16 +1,63 @@
-import counterIcon from "./../../assets/icons/counter-2.png";
-import percentIcon from "./../../assets/icons/percent.png";
+import { motion } from "motion/react";
+import { useState } from "react";
+import arrow from "./../../assets/icons/arrow-2.png";
+import counterIcon from "./../../assets/icons/counter-3.png";
+import percentIcon from "./../../assets/icons/percent-2.png";
+
+const menuVariants = {
+  closed: {
+    filter: "invert(100%)",
+  },
+  open: {
+    paddingTop: "10px",
+    filter: "invert(0%)",
+  },
+};
+
+const openMenuButtonVariants = {
+  closed: { rotateX: 0 },
+  open: { rotateX: 180, y: 10 },
+};
 
 const Menu = ({ openCounter, openPercent }) => {
+  const [open, setOpen] = useState(false);
+
+  const toggle = () => {
+    setOpen(!open);
+  };
+
+  const renderOpenMenu = () => {
+    return (
+      <>
+        <button className="menu-button" onClick={openCounter}>
+          <img className="menu-button-icon" src={counterIcon}></img>
+        </button>
+        <button className="menu-button" onClick={openPercent}>
+          <img className="menu-button-icon" src={percentIcon}></img>
+        </button>
+      </>
+    );
+  };
+
   return (
-    <div className="menu">
-      <button className="menu-button" onClick={openCounter}>
-        <img className="menu-button-icon" src={counterIcon}></img>
-      </button>
-      <button className="menu-button" onClick={openPercent}>
-        <img className="menu-button-icon" src={percentIcon}></img>
-      </button>
-    </div>
+    <motion.div
+      className="menu"
+      variants={menuVariants}
+      initial="closed"
+      animate={open ? "open" : "closed"}
+      layout
+    >
+      {open && renderOpenMenu()}
+      <motion.button
+        className="menu-open-button"
+        onClick={toggle}
+        variants={openMenuButtonVariants}
+        initial="closed"
+        animate={open ? "open" : "closed"}
+      >
+        <img className="menu-open-button-icon" src={arrow} />
+      </motion.button>
+    </motion.div>
   );
 };
 
